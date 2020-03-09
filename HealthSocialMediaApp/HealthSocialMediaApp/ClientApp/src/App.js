aaacd49ceb3bb7ca92d78720139321aad05a6cc4
Kevin Lima <17132584@student.hhs.nl>
2020-03-09 09:04:35 +0000

    Displaying Post
    - Post are fetched from the api
    - Post are displayed in a card list
    
    Related work items: #794

--- HealthSocialMediaApp/HealthSocialMediaApp/ClientApp/src/App.js
+++ HealthSocialMediaApp/HealthSocialMediaApp/ClientApp/src/App.js
@@ -5,6 +5,8 @@ import { Home } from "./components/Home";
 import { ApplicationUser } from "./components/ApplicationUser";
 import ApiAuthorizationRoutes from "./components/api-authorization/ApiAuthorizationRoutes";
 import { ApplicationPaths } from "./components/api-authorization/ApiAuthorizationConstants";
+import { Posts } from "./components/Posts";
+import { Post } from "./components/Post";
 
 export default class App extends Component {
 	static displayName = App.name;
@@ -14,6 +16,8 @@ export default class App extends Component {
 			<Layout>
 				<Route exact path="/" component={Home} />
 				<Route path="/account" component={ApplicationUser} />
+				<Route path="/posts" component={Posts} />
+				<Route path="/post/:id" component={Post} />
 				<Route
 					path={ApplicationPaths.ApiAuthorizationPrefix}
 					component={ApiAuthorizationRoutes}
---
+++ HealthSocialMediaApp/HealthSocialMediaApp/ClientApp/src/components/Post.js
@@ -0,0 +1,152 @@
+import React, { useState, useEffect } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import clsx from "clsx";
+import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
+import CardMedia from "@material-ui/core/CardMedia";
+import CardContent from "@material-ui/core/CardContent";
+import CardActions from "@material-ui/core/CardActions";
+import Collapse from "@material-ui/core/Collapse";
+import Avatar from "@material-ui/core/Avatar";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import { red } from "@material-ui/core/colors";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import ShareIcon from "@material-ui/icons/Share";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import Container from "@material-ui/core/Container";
+
+const useStyles = makeStyles(theme => ({
+	root: {
+		maxWidth: 600,
+		marginBottom: "60px"
+	},
+	media: {
+		height: 0,
+		paddingTop: "100%"
+	},
+	expand: {
+		transform: "rotate(0deg)",
+		marginLeft: "auto",
+		transition: theme.transitions.create("transform", {
+			duration: theme.transitions.duration.shortest
+		})
+	},
+	expandOpen: {
+		transform: "rotate(180deg)"
+	},
+	avatar: {
+		backgroundColor: red[500]
+	}
+}));
+
+const Post = () => {
+	const classes = useStyles();
+	const [expanded, setExpanded] = React.useState(false);
+
+	const handleExpandClick = () => {
+		setExpanded(!expanded);
+	};
+	const [data, setData] = useState({
+		posts: [],
+		isLoading: false,
+		error: undefined,
+		postId: this.props.match.params.id
+	});
+
+	useEffect(() => {
+		setData({
+			posts: [],
+			isLoading: true,
+			error: undefined
+		});
+		fetch("/api/post/" + data.postId, {})
+			.then(response => {
+				return response.json();
+			})
+			.then(post => {
+				setData({
+					posts: post,
+					isLoading: false,
+					error: undefined
+				});
+			})
+			.catch(error => {
+				console.log(error);
+			});
+	}, [setData]);
+	return (
+		<Container maxWidth="sm">
+			<h1>Explore</h1>
+			{data.isLoading && <Typography>Loading posts...</Typography>}
+			{!data.isLoading && data.error && (
+				<Typography>Could not fetch posts</Typography>
+			)}
+			{data.posts.map((item, i) => (
+				<Card className={classes.root}>
+					<CardHeader
+						avatar={
+							<Avatar
+								aria-label="recipe"
+								className={classes.avatar}
+							>
+								JD
+							</Avatar>
+						}
+						action={
+							<IconButton aria-label="settings">
+								<MoreVertIcon />
+							</IconButton>
+						}
+						title="John Doe"
+						subheader={item.createdAt}
+					/>
+					<CardMedia
+						className={classes.media}
+						image={item.imageLink}
+						title={item.description}
+					/>
+					<CardContent>
+						<Typography
+							variant="body2"
+							color="textSecondary"
+							component="p"
+						>
+							{item.description}
+						</Typography>
+					</CardContent>
+					<CardActions disableSpacing>
+						<IconButton aria-label="add to favorites">
+							<FavoriteIcon />
+						</IconButton>
+						<IconButton aria-label="share">
+							<ShareIcon />
+						</IconButton>
+						<IconButton
+							className={clsx(classes.expand, {
+								[classes.expandOpen]: expanded
+							})}
+							onClick={handleExpandClick}
+							aria-expanded={expanded}
+							aria-label="show more"
+						>
+							<ExpandMoreIcon />
+						</IconButton>
+					</CardActions>
+					<Collapse in={expanded} timeout="auto" unmountOnExit>
+						<CardContent>
+							<Typography paragraph>Comments:</Typography>
+							<Typography paragraph>
+								Comments are turned of.
+							</Typography>
+						</CardContent>
+					</Collapse>
+				</Card>
+			))}
+			;
+		</Container>
+	);
+};
+
+export { Post };
