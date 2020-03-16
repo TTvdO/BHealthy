84e129e07e75aa981ddabf08d41a73f2f7443411
Jari van Menxel <17030072@student.hhs.nl>
2020-03-16 14:39:19 +0000

    Merged PR 163: Enable automatic migrations
    Related work items: #1460

--- HealthSocialMediaApp/HealthSocialMediaApp/Startup.cs
+++ HealthSocialMediaApp/HealthSocialMediaApp/Startup.cs
@@ -1,9 +1,6 @@
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.EntityFrameworkCore;
 using HealthSocialMediaApp.Models;
@@ -27,7 +24,6 @@ namespace HealthSocialMediaApp
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
 
@@ -54,7 +50,6 @@ namespace HealthSocialMediaApp
             services.AddControllersWithViews();
             services.AddRazorPages();
 
-            // In production, the React files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
             {
                 configuration.RootPath = "ClientApp/build";
@@ -70,7 +65,6 @@ namespace HealthSocialMediaApp
             }
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -81,9 +75,12 @@ namespace HealthSocialMediaApp
             else
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            using (var scope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
+            using (var context = scope.ServiceProvider.GetService<ApplicationDbContext>())
+                context.Database.Migrate();
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
