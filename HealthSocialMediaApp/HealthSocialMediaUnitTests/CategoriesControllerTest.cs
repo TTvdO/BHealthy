ebfac35b4140027de35d2bf29079d198f7178686
Willem Delfos <15090604@student.hhs.nl>
2020-04-02 10:01:40 +0000

    Merged PR 554: Added test coverage posts controller categories controller and refactord code
    Related work items: #2310

---
+++ HealthSocialMediaApp/HealthSocialMediaUnitTests/CategoriesControllerTest.cs
@@ -0,0 +1,55 @@
+﻿using HealthSocialMediaApp.Controllers;
+using HealthSocialMediaApp.Data;
+using HealthSocialMediaApp.Models;
+using IdentityServer4.EntityFramework.Options;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace HealthSocialMediaUnitTest
+{
+	public class CategoriesControllerTest
+	{
+        public ApplicationDbContext CreateContextTests(string name)
+        {
+
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: name)
+                .Options;
+
+            var operationalStoreOptions = Options.Create(
+                new OperationalStoreOptions
+                {
+                    DeviceFlowCodes = new TableConfiguration("DeviceCodes"),
+                    PersistedGrants = new TableConfiguration("PersistedGrants")
+                });
+
+            return new ApplicationDbContext(options, operationalStoreOptions);
+        }
+
+        [Fact]
+        public async void DeletePost()
+        {
+            ApplicationDbContext _context = CreateContextTests("tesDb6");
+            CategoriesController c = new CategoriesController(_context);
+
+            _context.Categories.Add(new Category { Name="food"} );
+            _context.SaveChanges();
+
+            var result = await c.GetCategories();
+            List<string> listIds = new List<string>();
+            foreach (var category in result.Value)
+            {
+                var dictionary = new RouteValueDictionary(category);
+                listIds.Add(dictionary["Name"] as string);
+            }
+            
+            Assert.Equal("food",listIds.First());
+        }
+    }
+}
--- HealthSocialMediaApp/HealthSocialMediaUnitTests/PostsControllerTest.cs
+++ HealthSocialMediaApp/HealthSocialMediaUnitTests/PostsControllerTest.cs
@@ -9,13 +9,15 @@ using System.Linq;
 using HealthSocialMediaApp.Controllers;
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Routing;
+using Moq;
+using Microsoft.AspNetCore.Hosting;
 
 namespace HealthSocialMediaUnitTest
 {
     public class PostsControllerTests
     {
-
-        public ApplicationDbContext CreateContextTests(string name)
+		#region setup
+		public ApplicationDbContext CreateContextTests(string name)
         {
 
             var options = new DbContextOptionsBuilder<ApplicationDbContext>()
@@ -30,6 +32,28 @@ namespace HealthSocialMediaUnitTest
                 });
 
             return new ApplicationDbContext(options, operationalStoreOptions);
+        }
+
+        public Post CreateDummyData(ApplicationDbContext _context) {
+            var user = CreateDummyUser("Person", _context);
+            var category = CreateDummyCategory(12, _context);
+            var description = "My new shoes";
+            var imageLink = "../images/example.jpg";
+            DateTime createdAt = new DateTime(2019, 3, 25);
+
+            var post = new Post
+            {
+                Id = 12,
+                ApplicationUserId = user.Id,
+                CategoryId = category.Id,
+                Description = description,
+                ImageLink = imageLink,
+                CreatedAt = createdAt
+            };
+
+            _context.Posts.Add(post);
+            _context.SaveChanges();
+            return post;
         }
 
         private ApplicationUser CreateDummyUser(string id, ApplicationDbContext _context)
@@ -55,30 +79,14 @@ namespace HealthSocialMediaUnitTest
             _context.SaveChanges();
             return category;
         }
+		#endregion
 
-        [Fact]
+		[Fact]
         public void PostExists()
         {
             ApplicationDbContext _context = CreateContextTests("tesDb1");
-            var user = CreateDummyUser("sdfaf sdf", _context);
-            var category = CreateDummyCategory(12, _context);
-            var description = "My new shoes";
-            var imageLink = "../images/example.jpg";
-            DateTime createdAt = new DateTime(2019, 3, 25);
-
-            var post = new Post
-            {
-                Id = 12,
-                ApplicationUserId = user.Id,
-                CategoryId = category.Id,
-                Description = description,
-                ImageLink = imageLink,
-                CreatedAt = createdAt
-            };
-
-            _context.Posts.Add(post);
-            _context.SaveChanges();
-
+            var post = CreateDummyData(_context);
+            
             Assert.True(_context.Posts.Any(e => e.Id == post.Id));
         }
 
@@ -86,7 +94,7 @@ namespace HealthSocialMediaUnitTest
         public async void PostsInCorrectOrder()
         {
             ApplicationDbContext _context = CreateContextTests("testDb2");
-            var user = CreateDummyUser("sdfaf sdf", _context);
+            var user = CreateDummyUser("Danny", _context);
             var category = CreateDummyCategory(12, _context);
             var description = "My new shoes";
             var imageLink = "../images/example.jpg";
@@ -123,13 +131,66 @@ namespace HealthSocialMediaUnitTest
             List<Nullable<int>> listIds = new List<Nullable<int>>();
             foreach (var post in posts.Value)
             {
-                //code to get parameter of an anonymous object (System.Collections.Generic in Controller made it hard to get specific attribute of the object found).
-                //see https://stackoverflow.com/a/14877416 + https://docs.microsoft.com/en-us/dotnet/api/system.web.routing.routevaluedictionary?view=netframework-4.8
                 var dictionary = new RouteValueDictionary(post);
                 listIds.Add(dictionary["Id"] as Nullable<int>);
             }
 
             Assert.Equal(15, listIds.First());
+        }
+
+        [Fact]
+        public async void GetPostById()
+        {
+            ApplicationDbContext _context = CreateContextTests("tesDb3");
+            var Expected = CreateDummyData(_context);
+            PostsController postsController = new PostsController(_context, null);
+
+            var Result = await postsController.GetPost(Expected.Id);
+
+            Assert.Equal(Expected,Result.Value);
+        }
+
+        [Fact]
+        public async void LikeAPost()
+        {
+            ApplicationDbContext _context = CreateContextTests("tesDb4");
+            var post = CreateDummyData(_context);
+            PostsController postsController = new PostsController(_context, null);
+
+            await postsController.PutPostLike(post.Id,post.ApplicationUserId);
+
+            var likeCount = (from like in _context.Likes where like.PostId == post.Id select like.Id).Count();
+
+            Assert.Equal(1,likeCount);
+        }
+
+        [Fact]
+        public async void UnLikeAPost()
+        {
+            ApplicationDbContext _context = CreateContextTests("tesDb5");
+            var post = CreateDummyData(_context);
+            PostsController postsController = new PostsController(_context, null);
+            _context.Likes.Add(new Like { ApplicationUserId = post.ApplicationUserId, PostId = post.Id });
+
+            await postsController.PutPostUnlike(post.Id, post.ApplicationUserId);
+
+            var likeCount = (from like in _context.Likes where like.PostId == post.Id select like.Id).Count();
+
+            Assert.Equal(0, likeCount);
+        }
+
+        [Fact]
+        public async void DeletePost()
+        {
+            ApplicationDbContext _context = CreateContextTests("tesDb6");
+            var post = CreateDummyData(_context);
+            var hostingEnvironment = Mock.Of<IWebHostEnvironment>(e => e.ApplicationName == "application");
+            hostingEnvironment.WebRootPath = "../";
+            PostsController postsController = new PostsController(_context, hostingEnvironment);
+            
+            await postsController.DeletePost(post.Id);
+
+            Assert.False(_context.Posts.Any(e => e.Id == post.Id));
         }
     }
 }
